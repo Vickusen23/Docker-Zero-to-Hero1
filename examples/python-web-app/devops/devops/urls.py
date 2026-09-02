efd9bfbe8ef90108787a460df1583aa74b0d45c8
Victor Pius Usen <vickusen1@gmail.com>
2026-09-02 16:39:15 +0100

    Just updated urls.py

--- examples/python-web-app/devops/devops/urls.py
+++ examples/python-web-app/devops/devops/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('demo/', include('demo.urls')),
     path('admin/', admin.site.urls),
+    path('', home, name='home'),  # handles localhost:8000/
 ]
